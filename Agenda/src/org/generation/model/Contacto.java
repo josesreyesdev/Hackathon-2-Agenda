d888f832b23bcbf9d0de3a3a77703d729471cc0c
Deyanira Ruiz <drl300620@gmail.com>
2025-11-03 11:15:27 -0600

    Contacto Model Completo

--- Agenda/src/org/generation/model/Contacto.java
+++ Agenda/src/org/generation/model/Contacto.java
@@ -1,4 +1,46 @@
 package org.generation.model;
 
 public class Contacto {
+    private String Nombre;
+    private String Apellido;
+    private String nuevoTelefono;
+
+    public Contacto(String Nombre, String Apellido, String nuevoTelefono){
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.nuevoTelefono = nuevoTelefono;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getNuevoTelefono() {
+        return nuevoTelefono;
+    }
+
+    public void setNuevoTelefono(String nuevoTelefono) {
+        this.nuevoTelefono = nuevoTelefono;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Contacto{" +
+                "Nombre='" + Nombre + '\'' +
+                ", Apellido='" + Apellido + '\'' +
+                ", nuevoTelefono='" + nuevoTelefono + '\'' +
+                '}';
+    }
 }
